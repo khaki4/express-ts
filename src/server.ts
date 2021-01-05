52b6331925b3205565b6508909ceb1a46cd28f88
jiwonjung <4dakto@gmail.com>
2021-01-06 00:27:45 +0900

    connecting to MongoDb

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,22 @@
 import App from './app';
 import PostsController from './posts/posts.controller';
+import * as mongoose from 'mongoose';
+import 'dotenv/config';
+
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_PATH,
+} = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`
+
+mongoose.connect(uri, options);
+
 
 const app = new App(
   [
